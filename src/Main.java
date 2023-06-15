bc903e5fdb286a32553826a5d9ff6a77a354f514
Anirudha Biswal <anirudha.biswal@ibm.com>
2023-06-15 18:25:32 +0530

    SecondCommit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Altgit iniIDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
